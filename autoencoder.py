312429e66a49f35008612f2728e0cb7ba5f6580b
Ahmad Qadri <arqchicago@gmail.com>
2021-11-12 19:18:24 -0500

    added a validation set to validate model being trained

--- autoencoder.py
+++ autoencoder.py
@@ -2,30 +2,34 @@ from keras.datasets import mnist
 import numpy as np
 from tensorflow import keras
 from tensorflow.keras import losses
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Activation, Dense, InputLayer
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 
-# loading mnist data
+# loading mnist data and splitting into training, validation, and testing sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_val = train_test_split(x_train, test_size=0.1, random_state=42)
 
 # flattening the dataset. x_train: 60,000 images, 784 pixels,  x_test: 10,000 images, 784 pixels 
-x_train, x_test = x_train.astype('float32') / 255., x_test.astype('float32') / 255.
+x_train, x_test, x_val = x_train.astype('float32') / 255., x_val.astype('float32') / 255., x_test.astype('float32') / 255.
 x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_val = x_val.reshape((len(x_val), np.prod(x_val.shape[1:])))
 x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 print(f'train set:  {x_train.shape}')
+print(f'validation set:  {x_val.shape}')
 print(f'test set:  {x_test.shape}')
 
 # creating the autoencoder model
 # input compressed to 64 dim (compression factor of 12.25)
-autoencoder = keras.Sequential()
+autoencoder = Sequential()
 autoencoder.add(InputLayer(input_shape=(784,), name='input'))
 autoencoder.add(Dense(64, activation='relu', name='encoder'))   #relu sigmoid
 autoencoder.add(Dense(784, activation='sigmoid', name='decoder'))
 
 autoencoder.compile(optimizer='adam', loss='mean_squared_error')
-autoencoder.fit(x_train, x_train, epochs=70, validation_split=0.15)
+autoencoder.fit(x=x_train, y=x_train, epochs=10, validation_data=(x_val, x_val))
 
 # decode images in the test set 
 decoded_images = autoencoder.predict(x_test)
